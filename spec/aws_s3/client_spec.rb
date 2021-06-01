799e19c854e02395bc6b8323cf67ec5761d90471
Aleksey Strizhak <alexei.mifrill.strizhak@gmail.com>
2021-06-02 00:42:13 +0500

    fix typo in spec/aws_s3/client_spec.rb

--- spec/aws_s3/client_spec.rb
+++ spec/aws_s3/client_spec.rb
@@ -166,7 +166,7 @@ describe ArchivedRemoteObject::AwsS3::Client do
       end
 
       context "with API request" do
-        it 'fires external head_object request and returns "true" boolean' do
+        it 'fires external head_object request and returns "false" boolean' do
           expect(client).to receive(:bucket).once.and_return("bucket")
           s3_client.stub_responses(:head_object,
                                    status_code: 404,
